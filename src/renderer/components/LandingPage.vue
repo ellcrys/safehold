8c474d3056769ddf7d0ec8e65407728664d24a30
Kennedy Idialu <kennedyidialu@gmail.com>
2019-02-18 20:19:32 +0100

    Initial commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="wrapper"></div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'landing-page',
+	components: {},
+	methods: {},
+};
+</script>
+
+<style>
+</style>
