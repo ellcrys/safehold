64784653af151253b5da395d87d0cf1df3c19da6
Kennedy Idialu <kennedyidialu@gmail.com>
2019-03-12 16:54:45 +0100

    Updated test directory structure

---
+++ test/unit/specs/mixins/mixin.spec.ts
@@ -0,0 +1,7 @@
+import Mixin from "../../../../src/renderer/components/dashboard/Mixin";
+
+describe("Mixin", () => {
+	describe(".encrypt & decrypt", () => {
+		Mixin.methods.percentageDiff("100", "50");
+	});
+});
--- test/unit/specs/utilities/crypto.spec.js
+++ test/unit/specs/utilities/crypto.spec.js
@@ -22,7 +22,8 @@ describe("Encrypt", () => {
                 data: "abc",
                 outLen: 64,
                 salt: "",
-                out: "6b74330202f5af2987ec7ee364c570a6c784be6e433160d10efa91724f0f1383c127178d0f917afbcda64cb8f9cd74fd08c040dd0295c6b5e52546889da3af27",
+                out: "6b74330202f5af2987ec7ee364c570a6c784be6e433160d10efa91724f0f138" +
+                    "3c127178d0f917afbcda64cb8f9cd74fd08c040dd0295c6b5e52546889da3af27",
             },
             {
                 data: "xyz",
