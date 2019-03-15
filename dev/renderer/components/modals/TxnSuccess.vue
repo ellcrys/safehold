b919c9d59e16672aeea86b521c46b495dfe98ad1
Kennedy Idialu <kennedyidialu@gmail.com>
2019-03-15 17:23:17 +0100

    Fixed merged conflict

---
+++ dev/renderer/components/modals/TxnSuccess.vue
@@ -0,0 +1,88 @@
+<template>
+  <transition name="fade">
+    <div class="modal-overlay" id="overlay" v-if="open">
+      <div class="modal-pane">
+
+
+        <!-- Transaction Success Comienza  -->
+
+        <div class="" id="transaction-success-wrapper">
+
+            <div class="overlay-content">
+
+                <div class="overlay-header">
+                    <h1>Transaction Successful</h1>
+                    <button class="overlay-close"></button>
+                </div>
+
+                <div class="overlay-main">
+
+                    <img class="overlay-large-image" src="../../assets/img/crypto.svg" />
+
+                    <h2 class="overlay-heading-2">Transaction successful!</h2>
+
+                    <p>You sent <strong>138.994 ELL</strong> to:</p>
+
+                    <p><span>eCAMMWp4SERey2QJybU1Dmw8tRb6Y57uAL</span></p>
+                    <p>from your account:</p>
+                    <p><span class="active">eCAMMWp4SERey2QJybU1Dmw8tRb6Y57uAL</span></p>
+
+                    <div id="transaction-size">
+                        <span><em>Tnx</em> size 2.3kb</span> 
+                        <a href="">View txn on Ellscan</a>
+                    </div>
+
+
+
+                    <div class="txn-hash">
+                        <span>Txn Hash:</span>
+                        <div>
+                            <p>0xa09046714c3a3ebdd26a6d8dd5affa5d43272416845c388a9c6eccb4f0948102</p>
+                            <button>Copy</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <!-- Transaction Success Extremos -->
+
+
+
+
+      </div>
+    </div>
+  </transition>
+</template>
+
+
+<script lang="ts">
+import {
+	ModalTxnSuccessOpen,
+	ModalTxnSuccessClose,
+} from '../constants/events';
+
+
+export default {
+	data() {
+		return {
+			open: false,
+		};
+	},
+	created() {
+		this.$bus.$on(ModalTxnSuccessOpen, seedWords => {
+			this.open = true;
+		});
+
+		this.$bus.$on(ModalTxnSuccessClose, () => {
+			this.open = false;
+		});
+	},
+	methods: {
+		
+	},
+};
+</script>
--- src/renderer/components/modals/Modal.vue
+++ src/renderer/components/modals/Modal.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <ConfirmCopy/>
-    <ReceiveTxn/>
-    <SendTxn/>
+		<ReceiveTxn />
+		<SendTxn />
+		<ExportKey />
+		<ImportKey />
+		<NewAccount />
+		<SigninSuccess />
+		<TxnSuccess />
   </div>
 </template>
 
@@ -10,12 +15,22 @@
 import ConfirmCopy from './ConfirmCopy.vue';
 import ReceiveTxn from './ReceiveTxn.vue';
 import SendTxn from './SendTxn.vue';
+import ExportKey from './ExportKey.vue';
+import ImportKey from './ImportKey.vue';
+import NewAccount from './NewAccount.vue';
+import SigninSuccess from './SigninSuccess.vue';
+import TxnSuccess from './TxnSuccess.vue';
 
 export default {
 	components: {
 		ConfirmCopy,
 		ReceiveTxn,
 		SendTxn,
+		ExportKey,
+		ImportKey,
+		NewAccount,
+		SigninSuccess,
+		TxnSuccess,
 	},
 
 	data() {
